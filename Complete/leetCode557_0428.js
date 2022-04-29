dcd05464def6185e6dcb7e7e6aa6bf77283e4aab
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-04-29 22:58:50 +0900

    Create leetCode557_0428.js

---
+++ Complete/leetCode557_0428.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let words = s.split(' ');
+
+    for(let i = 0; i< words.length; i++){
+        words[i] = words[i].split('').reverse().join('');
+    }
+    
+    return words.join(' ');
+};
+
