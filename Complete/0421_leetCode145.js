ee7d9ac515e08a595aaded60a665fb84edf6d017
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-05-19 22:56:33 +0900

    Binary Tree Postorder Traversal [DFS]

--- Complete/0421_leetCode145.js
+++ Complete/0421_leetCode145.js
@@ -29,3 +29,30 @@ var postorderTraversal = function(root) {
     }
     return result;
 };
+
+//------------------------------------------------------------------------------
+//0519 두번째 시도 성공
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+    const result = [];
+    traversal(root);
+    function traversal(root){
+        if(!root) return;
+
+        traversal(root.left);
+        traversal(root.right);
+        result.push(root.val);
+    }
+    return result;
+};
