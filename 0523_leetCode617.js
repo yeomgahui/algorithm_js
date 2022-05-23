9e04e28efa6e35efa4372bfdfdda7a6dde546157
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-05-23 23:05:08 +0900

    Merge Two Binary Trees [DFS/BFS]

--- 0523_leetCode617.js
+++ 0523_leetCode617.js
@@ -11,6 +11,7 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
+// ---------------------------------------------- DFS --------------------------------------
 var mergeTrees = function(root1, root2) {
     if(!root1) return root2;
     if(!root2) return root1;
@@ -21,4 +22,29 @@ var mergeTrees = function(root1, root2) {
     root1.right = mergeTrees(root1.right, root2.right);
 
     return root1;
+};
+// ---------------------------------------------- BFS --------------------------------------
+const mergeTrees = function(root1, root2) {
+    if (!root1) return root2
+    if (!root2) return root1
+    
+    const res = root1
+    
+    const queue = []
+    queue.push({v1:res, v2:root2}) 
+    
+    while (queue.length) {
+        const {v1, v2} = queue.shift()
+        v1.val += v2.val
+        
+        if (v1.left && v2.left) queue.push({v1:v1.left, v2:v2.left})
+        if (!v1.left && v2.left) v1.left = v2.left
+        
+        if (v1.right && v2.right) queue.push({v1:v1.right, v2:v2.right})
+        if (!v1.right && v2.right) v1.right = v2.right  
+        
+    }
+    
+    return res
+     
 };
