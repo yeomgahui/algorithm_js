f44caf280504f4d74bb226001420d84c4967b58e
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-05-24 23:27:27 +0900

    Maximum Depth of Binary Tree [DFS/BFS]

--- 0414_leetcode104.js
+++ 0414_leetcode104.js
@@ -16,3 +16,16 @@ var maxDepth = function(root) {
     }
     return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
 };
+
+//----------------------- BFS -----------------------------
+var maxDepth = function(root) {
+    if(!root) return 0;
+    let queue = [{node: root, level: 1}], maxDepth = 0;
+    while(queue.length > 0){
+        let {node, level} = queue.shift();
+        maxDepth = Math.max(maxDepth, level);
+        if(node.left) queue.push({node: node.left, level: level+1});
+        if(node.right) queue.push({node: node.right, level: level+1})
+    }
+    return maxDepth;
+}
