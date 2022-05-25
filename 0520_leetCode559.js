c3244e91e83cc5ff9cb086eda2e95ab59ec8c16d
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2022-05-25 22:50:54 +0900

    Maximum Depth of N-ary Tree[BFS/DFS]

--- 0520_leetCode559.js
+++ 0520_leetCode559.js
@@ -6,3 +6,21 @@ var maxDepth = function(root) {
     }
     return max + 1;
 }
+
+// -------------------------- BFS ---------------------------
+var maxDepth = function(root) {
+    if(!root) return 0;
+    let depth = 0;
+    let queue = [{node : root, level: 1}];
+    
+    while(queue.length){
+        let {node, level} = queue.shift();
+        depth = Math.max(depth,level);
+        for(let children of node.children){
+            queue.push({node: children, level: level+1});
+        }
+    }
+    return depth;
+    
+    
+};
